5c866bc9998a3deb0daa5f3d46c0d84b7fc51cbe
Plamen Dimitrov <plamen.d@protonmail.com>
2023-09-16 15:44:56 +0300

    - weather widget styling

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -18,6 +18,25 @@ interface IWeatherData {
 	utc_offset_seconds: number;
 }
 
+const backgroundImages = [
+	{
+		id: 'nighttime-sky',
+		img: require('../src/img/cloudy-1869753_640.jpg'),
+	},
+	{
+		id: 'daytime-clear-sky',
+		img: require('../src/img/sun-3588618_640.jpg'),
+	},
+	{
+		id: 'daytime-cloudy-sky',
+		img: require('../src/img/sky-1494656_640.jpg'),
+	},
+	{
+		id: 'daytime-snowy-ground',
+		img: require('../src/img/snow-5707452_640.jpg'),
+	},
+];
+
 const Weather: React.FC = () => {
 	const [weatherData, setWeatherData] = useState<IWeatherData | undefined>();
 
@@ -35,7 +54,29 @@ const Weather: React.FC = () => {
 
 	const currentWeather = weatherData?.current_weather;
 
-	return <section className='weather'>{currentWeather?.temperature}</section>;
+	const displayBackgroundImage = () => {
+		let imageId = '';
+		if (currentWeather?.is_day !== 1) {
+			imageId = 'nighttime-sky';
+		} else {
+			if (currentWeather?.temperature > 18) {
+				imageId = 'daytime-clear-sky';
+			} else if (currentWeather?.temperature < 0) {
+				imageId = 'daytime-snowy-ground';
+			} else {
+				imageId = 'daytime-cloudy-sky';
+			}
+		}
+		const imageToDisplay = backgroundImages.find((item) => item.id === imageId);
+		return <img alt={imageToDisplay?.id} src={imageToDisplay?.img}></img>;
+	};
+
+	return (
+		<section className='weather'>
+			{displayBackgroundImage()}
+			<div className='temp'>{currentWeather?.temperature}&#176;</div>
+		</section>
+	);
 };
 
 export default Weather;
